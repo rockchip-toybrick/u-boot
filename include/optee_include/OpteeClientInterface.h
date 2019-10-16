fccae55ad70357e555a74bf3423a3f713dac61a2
Hisping Lin <hisping.lin@rock-chips.com>
2019-10-16 11:48:43 +0800

    lib: optee_client: add R&W toybrick sn mac ac func
    Signed-off-by: Hisping Lin <hisping.lin@rock-chips.com>

--- include/optee_include/OpteeClientInterface.h
+++ include/optee_include/OpteeClientInterface.h
@@ -36,6 +36,8 @@ uint32_t trusty_write_vbootkey_hash(uint32_t *buf, uint32_t length);
 uint32_t trusty_read_vbootkey_enable_flag(uint8_t *flag);
 uint32_t trusty_write_toybrick_seed(uint32_t *buf);
 uint32_t trusty_read_toybrick_cpu_id(uint8_t *buf);
+uint32_t trusty_read_toybrick_SnMacAc(uint8_t *data, uint32_t size);
+uint32_t trusty_write_toybrick_SnMacAc(uint8_t *data, uint32_t size);
 uint32_t trusty_read_permanent_attributes_flag(uint8_t *attributes);
 uint32_t trusty_write_permanent_attributes_flag(uint8_t attributes);
 uint32_t trusty_attest_dh(uint8_t *dh, uint32_t *dh_size);
